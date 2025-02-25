76450b782ebbe0fc6ff7f373cf96b88090e43204
Wyson Cheng <wyson002@gmail.com>
2025-02-25 22:11:03 +0000

    work on plugin

--- my-todo-plugin/main.ts
+++ my-todo-plugin/main.ts
@@ -1,134 +1,117 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice } from "obsidian";
+import { PublicClientApplication, Configuration } from "@azure/msal-node";
+import * as fs from "fs";
+import * as path from "path";
+import * as dotenv from "dotenv";
+import * as express from "express";
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MyTodoPlugin extends Plugin {
+	private tokenFilePath: string;
+	private pluginDir: string;
+	private pca: PublicClientApplication;
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.initializePlugin();
 	}
 
-	onunload() {
+	initializePlugin() {
+		// Determine the plugin directory inside Obsidian's `.obsidian/plugins/` folder
+		this.pluginDir = path.join(this.app.vault.configDir, "plugins/my-todo-plugin");
+		console.log("Plugin directory: ", this.pluginDir);
 
+		this.tokenFilePath = path.join(this.pluginDir, "auth_token.json");
+		console.log("Token file path: ", this.tokenFilePath);
+
+		// Explicitly define the real development path for .env
+		const devEnvPath = "/home/yson/projects/sync-obsidian-todo-plugin/my-todo-plugin/.env";
+
+		if (fs.existsSync(devEnvPath)) {
+			dotenv.config({ path: devEnvPath });
+			console.log("Loaded environment variables from:", devEnvPath);
+		} else {
+			console.warn("Evironment file not found at:", devEnvPath);
+		}
+
+		// Load environment variables for authentication
+		const CLIENT_ID: string = process.env.CLIENT_ID ?? "";
+		const CLIENT_SECRET: string = process.env.CLIENT_SECRET ?? "";
+		console.log("Client ID: ", CLIENT_ID);
+		console.log("Client Secret: ", CLIENT_SECRET);
+		const AUTHORITY = "https://login.microsoftonline.com/consumers";
+
+		const config: Configuration = {
+			auth: {
+				clientId: CLIENT_ID,
+				authority: AUTHORITY,
+				clientSecret: CLIENT_SECRET,
+			},
+		};
+
+		console.log("MSAL Configuration: ", config);
+
+		// Initialize the PublicClientApplication instance
+		this.pca = new PublicClientApplication(config);
+
+		// Register a command in Obsidian to trigger the login process
+		this.addCommand({
+			id: "login-microsoft-todo",
+			name: "Login to Microsoft To-Do",
+			callback: async () => {
+				await this.loginUser();
+			},
+		});
+
+		new Notice("Microsoft To-Do Plugin Loaded!");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	async loginUser() {
+		try {
+			const authCodeUrlParameters = {
+				scopes: ["Tasks.ReadWrite"],
+				redirectUri: "http://localhost:5000",
+			};
+
+			console.log("Check point")
+			const authUrl = await this.pca.getAuthCodeUrl(authCodeUrlParameters);
+			new Notice("Open this URL in your browser to log in: " + authUrl);
+			console.log("Auth URL: ", authUrl);
+
+			// Start an Express server to catch the authorization code
+			const authCode = await new Promise<string>((resolve) => {
+				const app = express(); // no .default needed
+				const server = app.listen(5000, () => {
+					console.log("Listening on http://localhost:5000 for authentication response...");
+				});
+
+				// Provide explicit types for req/res
+				app.get("/", (req: express.Request, res: express.Response) => {
+					const code = req.query.code as string;
+					console.log("Auth code received:", code);
+					res.send("Authorization successful! You can close this tab.");
+					server.close();
+					resolve(code);
+				});
+			});
+			
+			console.log("Auth code: ", authCode);
+
+		} catch (error) {
+			console.error("Login Error:", error);
+			new Notice("❌ Login faile//d! Check the console for details.");
+		}
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
 
-	display(): void {
-		const {containerEl} = this;
 
-		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+
+
+
+
+
