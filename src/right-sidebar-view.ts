01ab341c693c69af38198fe6ef53cc2baae4760e
Wyson Cheng <wyson002@gmail.com>
2025-04-19 12:25:43 +0100

    fix(sidebar): fix completed task line css changing

--- src/right-sidebar-view.ts
+++ src/right-sidebar-view.ts
@@ -289,12 +289,13 @@ export class TaskSidebarView extends ItemView {
 		const tomorrow = new Date();
 		tomorrow.setDate(tomorrow.getDate() + 1);
 		const tomIso = tomorrow.toISOString().slice(0, 10);
+		if (status === "completed") return { text: date, cls: "task-due-date" };
 
 		if (date === iso) {
 			return { text: "Today", cls: "task-due-date-now" };
 		} else if (date === tomIso) {
 			return { text: "Tomorrow", cls: "task-due-date-tomorrow" };
-		} else if (date < iso && status === "notStarted") {
+		} else if (date < iso) {
 			return { text: date, cls: "task-due-date-past" };
 		} else return { text: date, cls: "task-due-date" };
 	}
